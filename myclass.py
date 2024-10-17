0177027b500bcc09764c9a9a1874b6331255b139
Varun Patel <rvjpatel99@gmail.com>
2024-10-17 12:59:23 -0700

    capitalized the first letter of every word in the first sentence

---
+++ myclass.py
@@ -0,0 +1,13 @@
+class MyClass:
+
+    def __init__(self):
+        self.num = 7
+        self.string_var = 'my name'
+
+    def get_num(self):
+        return self.string_var
+
+    def print_num_sqaure(self):
+        return self.num**2
+
+    
